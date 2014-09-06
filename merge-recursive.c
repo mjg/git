9c7f82bb9f16678856de2579a654dba676cfc331
Thomas Rast <tr@thomasrast.ch>
2014-09-06 19:57:06 +0200

    log --remerge-diff: show what the conflict resolution changed
    Git has --cc as a very fast inspection tool that shows a brief summary
    of what a conflicted merge "looks like", and -c/-m as "give me the
    full information" data dumps.
    
    But --cc actually loses information: if the merge lost(!) some changes
    from one side, that hunk would fully agree with the other side, and
    therefore be elided.  So --cc cannot be used to investigate mismerges.
    Indeed it is rather hard to find a merge that has lost changes, unless
    one knows where to look.
    
    The new option --remerge-diff is an attempt at filling this gap,
    admittedly at the cost of a lot of CPU cycles.  For each merge commit,
    it diffs the merge result against a recursive merge of the merge's
    parents.
    
    For files that can be auto-merged cleanly, it will typically show
    nothing.  However, it will make it obvious when the merge introduces
    extra changes.
    
    For files that result in merge conflicts, we diff against the
    representation with conflict hunks (what the user would usually see in
    the worktree).  So the diff will show what was changed in the conflict
    hunks to resolve the conflict.
    
    It still takes a bit of staring to tell an evil from a regular merge.
    But at least the information is there, unlike with --cc; and the
    output is usually much shorter than with -c.
    
    Signed-off-by: Thomas Rast <tr@thomasrast.ch>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- merge-recursive.c
+++ merge-recursive.c
@@ -3175,7 +3175,7 @@ int merge_recursive(struct merge_options *o,
 	}
 
 	discard_cache();
-	if (!o->call_depth)
+	if (!o->call_depth && o->use_ondisk_index)
 		read_cache();
 
 	o->ancestor = "merged common ancestors";
@@ -3273,6 +3273,7 @@ void init_merge_options(struct merge_options *o)
 	o->merge_rename_limit = -1;
 	o->renormalize = 0;
 	o->detect_rename = 1;
+	o->use_ondisk_index = 1;
 	merge_recursive_config(o);
 	merge_verbosity = getenv("GIT_MERGE_VERBOSITY");
 	if (merge_verbosity)
